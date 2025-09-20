fb8df2e5cb315f79ecfa3a301c855040d42bf488
superdrive2000 <mgquinga@gmail.com>
2025-09-19 20:55:23 -0500

    Actualización del archivo principal

--- app.py
+++ app.py
@@ -20,10 +20,10 @@ col1, col2 = st.columns([2, 1])
 with col1:
     with st.form("formulario_prediccion"):
         tipo_farmacia = st.selectbox("Tipo de Farmacia", ["PROPIA", "FRANQUICIA"])
+        sucursal = st.selectbox("Sucursal", ["MEDICITY", "ECONOMICA"])
         venta_mensual = st.number_input("Venta mensual", min_value=0.0, step=1000.0)
         transacciones = st.number_input("Transacciones mensuales", min_value=0, step=100)
-        headcount = st.slider("HeadCount", min_value=2, max_value=9, step=1)
-        sucursal = st.selectbox("Sucursal", ["MEDICITY", "ECONOMICA"])
+        headcount = st.slider("HeadCount", min_value=2, max_value=9, step=1)     
         cluster = st.number_input("Cluster", min_value=0, max_value=99, step=1)
 
         # Botón centrado y grande usando columnas internas
